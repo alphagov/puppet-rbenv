c1c595ee11f4165861491fb3b3ea7f19f53de8fb
Garima/Jim <igarimasingh@gmail.com>
2013-05-17 15:25:28 +0100

    [#42296389] Added rbenv to the path
    ref:
    * https://github.com/sstephenson/rbenv#understanding-shims
    * https://github.com/sstephenson/rbenv#choosing-the-ruby-version

--- files/etc/profile.d/rbenv.sh
+++ files/etc/profile.d/rbenv.sh
@@ -1,2 +1,6 @@
+# rbenv work by intercepting invocations of 'ruby' etc. with a shim which basically
+# dispatches to a specified version of ruby, therefore rbenv needs to be on the default path
+
 export RBENV_ROOT="/usr/lib/rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
 [ -d "$RBENV_ROOT" ] && eval "$(rbenv init -)"
