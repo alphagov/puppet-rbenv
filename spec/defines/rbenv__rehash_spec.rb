dae127e665c85aaf6f62b5e7abaa0a470afe253a
Garima Singh <igarimasingh@gmail.com>
2013-05-23 15:05:09 +0100

    Fixing rbenv rehash tests

--- spec/defines/rbenv__rehash_spec.rb
+++ spec/defines/rbenv__rehash_spec.rb
@@ -4,9 +4,9 @@ describe 'rbenv::rehash' do
   context 'Version 1.2.3-p456' do
     let(:title) { '1.2.3-p456' }
 
-    it { 
+    it {
       should contain_exec('rbenv rehash for 1.2.3-p456').with(
-        :environment => 'RBENV_VERSION=1.2.3-p456',
+        :environment => ['RBENV_VERSION=1.2.3-p456', 'RBENV_ROOT=/usr/lib/rbenv'],
         :refreshonly => true
       )
     }
