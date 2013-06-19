329260d57ae0dd052030c4cfa464b8d30635a2be
Dan Carley <dan.carley@gmail.com>
2013-06-19 08:01:17 +0100

    [#51506059] Refactor rbenv::version tests
    Split behaviours out to context groups. Makes it more explicit that we are
    testing for, e.g. environment variables being set. These shouldn't change
    based on params so I'm only testing against the default params (none) for
    now.

--- spec/defines/rbenv__version_spec.rb
+++ spec/defines/rbenv__version_spec.rb
@@ -3,39 +3,59 @@ require 'spec_helper'
 describe 'rbenv::version' do
   context 'Version 1.2.3-p456' do
     let(:title) { '1.2.3-p456' }
+    let(:exec_title) { 'bundler for 1.2.3-p456' }
 
-    it {
-      should contain_package('rbenv-ruby-1.2.3-p456').with(
-        :require => 'Class[Rbenv]'
-      )
-    }
-
-    context 'should install bundler without specifying version if bundler_version not provided' do
+    context 'ruby version' do
       it {
-        should contain_exec('bundler for 1.2.3-p456').with(
-          :command     => /^\/usr\/bin\/rbenv exec gem install bundler$/,
-          :unless      => /^RBENV_ROOT=\/usr\/lib\/rbenv RBENV_VERSION=1.2.3-p456 \/usr\/bin\/rbenv exec/,
+        should contain_package('rbenv-ruby-1.2.3-p456').with(
+          :require => 'Class[Rbenv]'
+        )
+      }
+    end
+
+    context 'rehash' do
+      it { should contain_rbenv__rehash('1.2.3-p456') }
+      it { should contain_exec(exec_title).with_notify('Rbenv::Rehash[1.2.3-p456]') }
+    end
+
+    context 'bunder' do
+      it 'should prefix commands with rbenv exec' do
+        should contain_exec(exec_title).with(
+          :command     => /\/usr\/bin\/rbenv exec gem /,
+          :unless      => /\/usr\/bin\/rbenv exec gem /
+        )
+      end
+
+      it 'should set env vars for rbenv' do
+        should contain_exec(exec_title).with(
+          :unless      => /^RBENV_ROOT=\/usr\/lib\/rbenv RBENV_VERSION=1.2.3-p456 /,
           :environment => [
             'RBENV_ROOT=/usr/lib/rbenv',
             'RBENV_VERSION=1.2.3-p456',
           ],
-          :provider => 'shell',
-          :notify => 'Rbenv::Rehash[1.2.3-p456]'
+          :provider    => 'shell'
         )
-      }
-    end
+      end
 
-    context 'should install bundler with specified version' do
-      let(:params) { {
-        :bundler_version => '8.9.0'
-      } }
-      it {
-        should contain_exec('bundler for 1.2.3-p456').with(
-          :command => /^\/usr\/bin\/rbenv exec gem install bundler -v 8.9.0$/
-        )
-      }
-    end
+      context 'bundler_version not set (default)' do
+        it {
+          should contain_exec(exec_title).with(
+            :command => /gem install bundler$/
+          )
+        }
+      end
 
-    it { should contain_rbenv__rehash('1.2.3-p456') }
+      context 'bundler_version 8.9.0' do
+        let(:params) {{
+          :bundler_version => '8.9.0'
+        }}
+
+        it {
+          should contain_exec(exec_title).with(
+            :command => /gem install bundler -v 8.9.0$/
+          )
+        }
+      end
+    end
   end
 end
